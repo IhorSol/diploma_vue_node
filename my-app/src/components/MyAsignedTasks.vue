729e1931adeafe49759ba17674a851bd3b587c57
Liliya Solohub <liliasolohub15@gmail.com>
2021-11-03 22:15:49 +0200

    changes my task page, not important

--- my-app/src/components/MyAsignedTasks.vue
+++ my-app/src/components/MyAsignedTasks.vue
@@ -1,21 +1,23 @@
 <template lang="html">
-  <div class="asigned_tasks">
+  <div>
     <h2>Counter. Tasks not viewed - {{ countNotViewedTasks.length }}</h2> <!-- показує к-сть непродивлених задач -->
-    <div class="asigned_task" v-for="item in myTasks" v-bind:key="item._id">
-      <div class="asigned_task__header">
-        <div class="asigned_task__complexity ">{{ item.complication }}</div> <!--/*******color_complexity -->
-        <div class="asigned_task__name"><h3>{{ item.title }} </h3></div>
-      </div>
-      <div class="asigned_task__body">
-        <p class="asigned_task_text">{{ item.taskDescription }}</p>
-        <div class="asigned_task__deadline"><i class="fas fa-calendar-week"></i> {{ item.deadline}}</div>
-        <div class="asigned_task__asigned-by"><i class="fas fa-user"></i>{{ allUsers[item.creator-1].name }}</div>
-      <div class="asigned_task__buttons">
-        <!-- <button id="btn_done">Виконано</button>
-        <button id="btn_more">Детальніше</button> -->
-        <button class="comment_btn" @click='transferDataToCommentForm(item)'><i class="fas fa-comment-dots"></i></button>
-        <button class="show_btn" @click='transferDataToShowForm(item); '><i class="fas fa-eye"></i></button>
-        <button id="done_btn" @click='completeTask(item._id)'><i class="far fa-check-circle"></i></button>
+    <div class="asigned_tasks">
+      <div class="asigned_task" v-for="item in myTasks" v-bind:key="item._id">
+        <div class="asigned_task__header">
+          <div class="asigned_task__complexity ">{{ item.complication }}</div> <!--/*******color_complexity -->
+          <div class="asigned_task__name"><h3>{{ item.title }} </h3></div>
+        </div>
+        <div class="asigned_task__body">
+          <p class="asigned_task_text">{{ item.taskDescription }}</p>
+          <div class="asigned_task__deadline"><i class="fas fa-calendar-week"></i> {{ item.deadline}}</div>
+          <div class="asigned_task__asigned-by"><i class="fas fa-user"></i>{{ allUsers[item.creator-1].name }}</div>
+        <div class="asigned_task__buttons">
+          <!-- <button id="btn_done">Виконано</button>
+          <button id="btn_more">Детальніше</button> -->
+          <button class="comment_btn" @click='transferDataToCommentForm(item)'><i class="fas fa-comment-dots"></i></button>
+          <button class="show_btn" @click='transferDataToShowForm(item); '><i class="fas fa-eye"></i></button>
+          <button id="done_btn" @click='completeTask(item._id)'><i class="far fa-check-circle"></i></button>
+        </div>
       </div>
     </div>
   </div>
